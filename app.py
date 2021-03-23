f004c31bf88336f3b5c87fd0e8395a1936fd9a68
CKChang <benctw@gmail.com>
2021-03-23 10:00:54 +0800

    buttons_template_message

--- app.py
+++ app.py
@@ -53,6 +53,46 @@ def handle_message(event):
     # get msg details
     print('msg from [', user_name, '](', user_id, ') : ', msg)
 
+    buttons_template_message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/w1JM4f5.jpg',
+            title='問一下唷~',
+            text='請問你想喝哪一種飲料',
+            actions=[
+                PostbackAction(
+                    label='晚點喝',
+                    display_text='我想晚點再喝',
+                    data='action=buy&itemid=1'
+                ),
+                MessageAction(
+                    label='我想喝茶',
+                    text='給我一杯茶吧'
+                ),
+                MessageAction(
+                    label='我想喝咖啡',
+                    text='比較想喝咖啡'
+                ),
+                URIAction(
+                    label='想查一下營養資訊',
+                    uri='tel:+886933333252'
+                )
+            ]
+        )
+    )
+
+
+    line_bot_api.reply_message(event.reply_token, buttons_template_message)
+
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    print(event.postback.data)
+    print(parse_qsl(event.postback.data))
+    print(dict(parse_qsl(event.postback.data)))
+    data=dict(parse_qsl(event.postback.data))
+    print(data['action'])
+
 # run app
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
