f9df850aa3bd2fc2f440b798e31fd11d7441fb97
CKChang <benctw@gmail.com>
2021-03-23 09:59:25 +0800

    location message

--- app.py
+++ app.py
@@ -53,6 +53,11 @@ def handle_message(event):
     # get msg details
     print('msg from [', user_name, '](', user_id, ') : ', msg)
 
+    line_bot_api.reply_message(event.reply_token, LocationSendMessage(title='Store Location',
+                                                                        address='Taipei 101',
+                                                                        latitude=25.033981,
+                                                                        longitude=121.564506))
+
 # run app
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
