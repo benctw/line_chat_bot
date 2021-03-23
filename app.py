1ae4fdc21acacf34d566a3129f8cd54ec3c2d442
CKChang <benctw@gmail.com>
2021-03-23 15:26:37 +0800

    travel

--- app.py
+++ app.py
@@ -53,16 +53,45 @@ def handle_message(event):
     # get msg details
     print('msg from [', user_name, '](', user_id, ') : ', msg)
 
-    json_path = os.path.join(os.path.split(__file__)[0], 'json.txt')
+    json_path = os.path.join(os.path.split(__file__)[0], 'json-area.txt')
     with open(json_path, 'r', encoding='UTF-8') as f:
-        flexmessagestring = f.read()
-    flexmessagedict = json.loads(flexmessagestring)
-    flexmessagedict['header']['contents'][0]['contents'][1]['text']='南港'
+        bubblestring = f.read()
+    bubbledict = json.loads(bubblestring)
+    bubbledictlist = []
+
+    urllist=['https://i.imgur.com/JERvLX3.jpg', 
+            'https://i.imgur.com/j5U7aWN.jpg', 
+            'https://i.imgur.com/1cvXCcL.jpg', 
+            'https://i.imgur.com/gkgvBJA.jpg']
+    contentstextlist=['宜花東飯店', 
+                    '台北桃竹苗飯店', 
+                    '台中雲彰飯店', 
+                    '高雄台南嘉義飯店']
+    buttoncontentstextlist=['搜尋宜花東飯店', 
+                    '搜尋台北桃竹苗飯店', 
+                    '搜尋台中雲彰飯店', 
+                    '搜尋高雄台南嘉義飯店']
+    actionlist=['action=step1&area=1', 
+                'action=step1&area=2', 
+                'action=step1&area=3', 
+                'action=step1&area=4']
+    
+    i=0
+    for _ in urllist:
+        bubbledict['body']['contents'][0]['url']=urllist[i]
+        bubbledict['body']['contents'][2]['contents'][0]['contents'][0]['contents'][0]['text']=contentstextlist[i]
+        bubbledict['body']['action']['data']=actionlist[i]
+        bubbledictlist.append(json.loads(json.dumps(bubbledict.copy()))) 
+        i+=1
+
+    flexmessagedict={"type":"carousel"}
+    flexmessagedict['contents']=bubbledictlist
+
     flex_message = FlexSendMessage(
         alt_text='hello',
         contents=flexmessagedict
     )
-
+    # print(flexmessagedict)
     line_bot_api.reply_message(event.reply_token, flex_message)
     
 # run app
