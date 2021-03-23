b05972707acadc636937517016c3da8ff22b4f3b
CKChang <benctw@gmail.com>
2021-03-23 09:58:22 +0800

    video message

--- app.py
+++ app.py
@@ -53,6 +53,9 @@ def handle_message(event):
     # get msg details
     print('msg from [', user_name, '](', user_id, ') : ', msg)
 
+    line_bot_api.reply_message(event.reply_token, VideoSendMessage(original_content_url='https://d18def1b3f81.ngrok.io/static/dog.mp4',
+                                                                    preview_image_url='https://d18def1b3f81.ngrok.io/static/dog.png'))
+
 # run app
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
