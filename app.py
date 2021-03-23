219c708dbb43c27989e36f8b060cc0c47ba9b234
CKChang <benctw@gmail.com>
2021-03-23 09:53:53 +0800

    reply

--- app.py
+++ app.py
@@ -53,6 +53,8 @@ def handle_message(event):
     # get msg details
     print('msg from [', user_name, '](', user_id, ') : ', msg)
 
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = msg))
+
 # run app
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
